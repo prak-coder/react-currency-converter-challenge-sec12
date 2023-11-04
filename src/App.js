31a2fba1b1937721a9556883c210c804b9765c78
Prakash K <prakash.change@gmail.com>
2023-11-04 12:12:28 +0530

    currency converter challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [input, setInput] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      if (fromCurrency === toCurrency) {
+        setOutput(input);
+        return;
+      }
+      async function getConvertedCurrency() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${input}&from=${fromCurrency}&to=${toCurrency}`
+        );
+        const data = await res.json();
+        // console.log(data.rates.INR);
+        setIsLoading(false);
+        setOutput(data.rates[toCurrency]);
+      }
+      getConvertedCurrency();
+    },
+    [input, fromCurrency, toCurrency]
+  );
+  function handleInput(e) {
+    setInput(Number(e.target.value));
+  }
+  function handleFromCurrency(e) {
+    setFromCurrency(e.target.value);
+  }
+  function handleToCurrency(e) {
+    setToCurrency(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInput}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={handleFromCurrency}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={handleToCurrency}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {output} {toCurrency}
+      </p>
     </div>
   );
 }
-
-export default App;
